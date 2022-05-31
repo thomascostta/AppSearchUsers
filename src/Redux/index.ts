b4341be56e9ff3af4e7161738b78eecc55c74115
thomascostta <thomas.jeffcosta@gmail.com>
2022-05-31 16:19:54 -0300

    redux config

---
+++ src/Redux/index.ts
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, Store, AnyAction } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store: Store<unknown, AnyAction> = createStore(
+    rootReducer,
+    applyMiddleware(sagaMiddleware)
+)
+
+sagaMiddleware.run(rootSaga)
+
+export default store
+export * from './actions'
+export * from './types'
