d723e5641a7eb329acbfd8cb8e7703e80dc5ae6d
thomascostta <thomas.jeffcosta@gmail.com>
2022-06-02 10:33:01 -0300

    settings

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import User from '../../components/user';
 
 const Home = () => {
   return (
-    <View >
-      <Text>Olá Brasil!</Text>
-    </View>
+    < >
+      <User />
+    </>
   );
 };
 
